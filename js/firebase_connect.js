a4ba7dd0e1093c0fc5f87fabb863278464cbf68c
pteacher <subfear@mail.ru>
2016-11-13 23:14:43 +0500

    initial commit

---
+++ js/firebase_connect.js
@@ -0,0 +1,9 @@
+﻿  var config = {
+    apiKey: "AIzaSyAUGf75YmzFtvxs8q2jIKqb4EC-cI9g2yI",
+    authDomain: "respekt-1b30b.firebaseapp.com",
+    databaseURL: "https://respekt-1b30b.firebaseio.com",
+    storageBucket: "respekt-1b30b.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database().ref();
